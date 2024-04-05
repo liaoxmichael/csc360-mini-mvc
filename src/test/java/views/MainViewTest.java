87d55ef5a555c8549586d838b76776224db1ac2d
Michael Liao <michael.liao@centre.edu>
2024-04-05 16:08:27 -0400

    beginning tests

--- src/test/java/views/MainViewTest.java
+++ src/test/java/views/MainViewTest.java
@@ -1,11 +1,15 @@
 package views;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.assertions.api.Assertions;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import main.Main;
@@ -14,7 +18,7 @@ import models.AdderModel;
 @ExtendWith(ApplicationExtension.class)
 public class MainViewTest
 {
-	
+
 	@Start
 	public void start(Stage stage) throws Exception
 	{
@@ -31,10 +35,30 @@ public class MainViewTest
 		stage.setScene(s);
 		stage.show();
 	}
-	
-	public MainViewTest()
+
+	private void enterAmount(FxRobot robot, int num, String target)
 	{
-		// TODO Auto-generated constructor stub
+		robot.clickOn(target);
+		robot.write(String.valueOf(num));
 	}
 
+	private void checkSum(FxRobot robot, int expected)
+	{
+		robot.clickOn("#addButt");
+		Assertions.assertThat(robot.lookup("#sum").queryAs(Label.class)).hasText(String.valueOf(expected));
+	}
+	
+	private void checkAddNums(FxRobot robot, int num1, int num2) {
+		enterAmount(robot, num1, "#num1");
+		enterAmount(robot, num2, "#num2");
+		checkSum(robot, num1+num2);
+	}
+	
+	@Test
+	public void testAdds(FxRobot robot) {
+		checkAddNums(robot, 1, 1);
+		checkAddNums(robot, -1, 1);
+		checkAddNums(robot, 10, 100);
+		checkAddNums(robot, 1, 1);
+	}
 }
